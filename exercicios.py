b775eb5a67a11bb1a656d0485750023a261625b3
Thiago Silva <conhecimentoevalor@gmail.com>
2025-03-16 16:20:58 -0300

    adicionado exercicos 10

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,10 @@
+import math 
+
 numero_01 = int(input("Inserir umnumero inteiro: "))
 numero_02 = int(input("Inserir outro numero inteiro: "))
 resultado = numero_01 // numero_01
 print(resultado)
+
+raio_do_circulo = float(input("Digite o raio: "))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+print(f"{area_do_circulo:.2f}")
